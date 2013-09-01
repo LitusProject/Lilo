69da791f5b590fa4c3ca7d08c99eac52cc55cdca
Kristof Marien <krmarien@gmail.com>
2013-09-01 22:37:49 +0200

    Added number unread to navbar

--- src/Lilo/AppBundle/Controller/IndexController.php
+++ src/Lilo/AppBundle/Controller/IndexController.php
@@ -27,7 +27,7 @@ class IndexController extends Controller
     public function indexAction(Instance $instance = null)
     {
         $since = new DateTime();
-        $since->sub(new DateInterval('P1W'));
+        $since->sub(new DateInterval('P2W'));
 
         $exceptions = $this->getDoctrine()->getManager()
             ->getRepository('Lilo\AppBundle\Document\Exception')
